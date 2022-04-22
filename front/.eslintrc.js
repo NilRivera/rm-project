503a2f7dfcd3f942dc11788f31ca27d3cef6e7c7
NilRivera <nil.rivera.ventura@gmail.com>
2022-04-22 16:53:35 +0200

    Add media and default config

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -3,7 +3,14 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:react/recommended', 'airbnb'],
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:import/typescript',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
@@ -14,17 +21,16 @@ module.exports = {
   },
   plugins: ['react', '@typescript-eslint'],
   settings: {
-    'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
-    'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx'],
-    },
     'import/resolver': {
       node: {
         extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        paths: ['src'],
       },
     },
   },
   rules: {
+    'react/require-default-props': [0],
+    'import/prefer-default-export': [0],
     'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.ts'] }],
     'react/function-component-definition': [
       2,
