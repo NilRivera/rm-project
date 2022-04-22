4eb01e5354f964f18084d247edfdc9222c7081ca
NilRivera <nil.rivera.ventura@gmail.com>
2022-04-22 16:56:44 +0200

    Add them constants

---
+++ front/src/styles/theme.ts
@@ -0,0 +1,25 @@
+const theme = {
+  color: {
+    white: '#FFFFFF',
+    black: '#000000',
+    grey: '#979797',
+    lightGrey: '#e0e0e0',
+  },
+  buttonHeight: {
+    small: '30px',
+    medium: '40px',
+    large: '50px',
+  },
+  buttonWidth: {
+    small: '60px',
+    medium: '70px',
+    large: '80px',
+  },
+  fontSize: {
+    small: '0.8em',
+    medium: '1.2em',
+    large: '1.5em',
+  },
+};
+
+export default theme;
