eee8415a11dfb34ff9428e63d012d196edebb134
NilRivera <nil.rivera.ventura@gmail.com>
2022-04-24 12:57:21 +0200

    Add character template

---
+++ front/src/global/constants.ts
@@ -0,0 +1,15 @@
+export const load = {
+  id: 1,
+  name: 'Loading...',
+  status: 'Loading...',
+  species: 'Loading...',
+  type: 'Loading...',
+  gender: 'Loading...',
+  origin: {
+    name: 'Loading...',
+  },
+  location: {
+    name: 'Loading...',
+  },
+  image: 'https://rickandmortyapi.com/api/character/avatar/19.jpeg',
+};
