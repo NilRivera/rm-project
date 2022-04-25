1a447500eb65d567802534fe7c6f36c66ac0bb49
NilRivera <nil.rivera.ventura@gmail.com>
2022-04-25 21:04:03 +0200

    Update eslint and create prettier

--- back/.eslintrc.js
+++ back/.eslintrc.js
@@ -4,12 +4,16 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ['airbnb-base'],
   parserOptions: {
     ecmaVersion: 'latest',
   },
   rules: {
+    'implicit-arrow-linebreak': [
+      0,
+      'as-needed',
+      { requireReturnForObjectLiteral: false },
+    ],
+    'function-paren-newline': [0, 'never'],
   },
 };
