ad13ceb8f14ef2f1bbcb0addbf645fc5c7eb882f
NilRivera <nil.rivera.ventura@gmail.com>
2022-04-25 21:25:08 +0200

    Add fake token - TODO

--- front/src/services/api/common/api-config.ts
+++ front/src/services/api/common/api-config.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
+import tokenInterceptor from './interceptors/token';
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_BASE_API_URL as string,
 });
+
+tokenInterceptor(api);
 
 export default api;
--- front/src/services/api/common/interceptors/token.ts
+++ front/src/services/api/common/interceptors/token.ts
@@ -1,2 +1,5 @@
-// Token
-export {};
+/* eslint-disable no-param-reassign */
+export default function tokenInterceptor(api) {
+  const fakeToken = 'user-token';
+  api.defaults.headers.Authorization = `Bearer ${fakeToken}`;
+}
